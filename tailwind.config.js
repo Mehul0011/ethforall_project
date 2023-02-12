078d567df1475bf5a57b9e1cabafc9a1a31cd7d1
Mehul0011 <imehul24@gmail.com>
2023-02-12 16:05:56 +0530

    feat: color palette added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,27 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        primary: "#00b89f",
+        background: "#24292d",
+        backgroundPaper: "#272e36",
+
+        // text colors
+        primaryText: '#fff',
+        contrastText: "#1a1a1a", 
+        secondaryText: "#6C7175",
+
+        //border
+        borderColor: "#9c9c9c"
+      }
+    }
   },
   plugins: [],
 }
