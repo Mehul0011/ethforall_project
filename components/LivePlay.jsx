4ee9de49fc953564a17e31c1d9594cc2bb32884b
RaveenaBhasin <raveenabhasin15@gmail.com>
2023-02-18 18:19:24 +0530

    fix: replace live replay component

--- components/LivePlay.jsx
+++ components/LivePlay.jsx
@@ -1,12 +1,16 @@
 import useSuperFluid from '@/hooks/useSuperFluid';
-import { useState, useEffect } from 'react';
+
+import { useMemo, useState, useEffect } from 'react';
 import useLivePeer from '@/hooks/useLivePeer';
+import { FiCopy } from 'react-icons/fi';
+import ReactPlayer from 'react-player';
 import { useAuth } from '@arcana/auth-react';
 import {
   ControlsContainer,
   PictureInPictureButton,
   PlayButton,
   Player,
+  Poster,
   Progress,
   TimeDisplay,
   Title,
@@ -16,7 +20,6 @@ import {
 export default function LivePlay() {
   const [streamName, setStreamName] = useState('');
   const [playbackIds, setPlaybackIds] = useState([]);
-  const [flowStarted, setFlowStarted] = useState(false);
   const livePeer = useLivePeer();
   const superFluid = useSuperFluid();
   const auth = useAuth();
@@ -29,6 +32,7 @@ export default function LivePlay() {
       setWalletAddress(auth.user?.address);
     }
   }, []);
+
 
   useEffect(() => {
     const getPlayBackIds = async () => {
@@ -44,11 +48,10 @@ export default function LivePlay() {
   }, []);
 
 
-  const startFlow = async () => {
-    console.log("super");
-    const res = await superFluid.createFlow('1', '0x6D91A519E6bfBA9482e51093b5C3113890b37541');
-    if(res)
-      setFlowStarted(true);
+  const startFlow = () => {
+    // setFlowLoading(true);
+    const res = superFluid.createFlow('1', '0x6D91A519E6bfBA9482e51093b5C3113890b37541');
+    // setFlowLoading(false);
     console.log("super",res);
   }
 
@@ -72,7 +75,7 @@ export default function LivePlay() {
   return (
     <div className="h-screen z-0 p-10">
       <h1 className="text-3xl font-bold pb-2 mb-4 ">Watch Live Streams</h1>
-      <div className=" grid grid-cols-2 items-start justify-center gap-4">
+      <div className="items-start justify-center">
         {
           playbackIds && playbackIds.map(stream => {
             if (stream.playbackId && stream.isActive) {
@@ -80,17 +83,21 @@ export default function LivePlay() {
                 <div key={stream.playbackId}>
                   <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
                     <a href="#">
-                      <div className="w-full h-2/3">
-                        <h1>Stream Name: {stream.name}</h1>
-                        <button onClick={startFlow}>Start Flow</button>
-                        {flowStarted && (
+                      <div className="w-full h-1/3">
+                        {/* <Player
+                          title={stream.name}
+                          playbackId={stream.playbackId}
+                          autoPlay
+                          muted
+                        />
+                        <h1>more props</h1> */}
                         <Player title={stream?.name} playbackId={stream?.playbackId}>
                           <Title/>
                           <ControlsContainer
                             middle={<Progress />}
                             left={
                               <>
-                                <PlayButton  />
+                                <PlayButton onClick={startFlow()} />
                                 <Volume showSlider={false} />
                                 <TimeDisplay />
                               </>
@@ -98,7 +105,6 @@ export default function LivePlay() {
                             right={<PictureInPictureButton />}
                           />
                         </Player>
-                        )}
                       </div>
                     </a>
                   </div>
@@ -114,3 +120,116 @@ export default function LivePlay() {
     </div>
   );
 };
+
+// import useSuperFluid from '@/hooks/useSuperFluid';
+// import { useState, useEffect } from 'react';
+// import useLivePeer from '@/hooks/useLivePeer';
+// import { useAuth } from '@arcana/auth-react';
+// import {
+//   ControlsContainer,
+//   PictureInPictureButton,
+//   PlayButton,
+//   Player,
+//   Progress,
+//   TimeDisplay,
+//   Title,
+//   Volume,
+// } from "@livepeer/react";
+
+// export default function LivePlay() {
+//   const [streamName, setStreamName] = useState('');
+//   const [playbackIds, setPlaybackIds] = useState([]);
+//   const [flowStarted, setFlowStarted] = useState(false);
+//   const livePeer = useLivePeer();
+//   const superFluid = useSuperFluid();
+//   const auth = useAuth();
+//   const [walletAddress, setWalletAddress] = useState('');
+//   const {createFlow} = useSuperFluid();
+//   const [flowLoading, setFlowLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (auth.isLoggedIn) {
+//       setWalletAddress(auth.user?.address);
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     const getPlayBackIds = async () => {
+//       try {
+//         const response = await livePeer.getAllStreams();
+//         setPlaybackIds(response);
+//         console.log("Live stream", response)
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+//     getPlayBackIds();
+//   }, []);
+
+
+//   const startFlow = async () => {
+//     console.log("super");
+//     const res = await superFluid.createFlow('1', '0x6D91A519E6bfBA9482e51093b5C3113890b37541');
+//     // if(res)
+//     //   setFlowStarted(true);
+//     console.log("super",res);
+//   }
+
+//   return (
+//     <div className="h-screen z-0 p-10">
+//       <h1 className="text-3xl font-bold pb-2 mb-4 ">Watch Live Streams</h1>
+//       <div className=" items-start justify-center">
+//         {
+//           playbackIds && playbackIds.map(stream => {
+//             if (stream.playbackId && stream.isActive) {
+//               return (
+//                 <div key={stream.playbackId}>
+//                   <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
+//                       <div className="w-full h-2/3">
+//                         {/* <button onClick={startFlow}>Start Flow</button> */}
+//                         {/* {flowStarted && (
+//                         <Player title={stream?.name} playbackId={stream?.playbackId}>
+//                           <Title/>
+//                           <ControlsContainer
+//                             middle={<Progress />}
+//                             left={
+//                               <>
+//                                 <PlayButton />
+//                                 <Volume showSlider={false} />
+//                                 <TimeDisplay />
+//                               </>
+//                             }
+//                             right={<PictureInPictureButton />}
+//                           />
+//                         </Player>
+//                         )} */}
+//                          <Player title={stream?.name} playbackId={stream?.playbackId}>
+//                           <Title/>
+//                           <ControlsContainer
+//                             middle={<Progress />}
+//                             left={
+//                               <>
+//                                 <PlayButton onClick={startFlow()}/>
+//                                 <Volume showSlider={false} />
+//                                 <TimeDisplay />
+//                               </>
+//                             }
+//                             right={<PictureInPictureButton />}
+//                           />
+//                         </Player>
+//                       </div>
+//                   </div>
+                  
+//                 </div>
+
+//               )
+//             } else {
+//               return null;
+//             }
+//           })
+//         }
+//       </div>
+//     </div>
+//   );
+// };
+
